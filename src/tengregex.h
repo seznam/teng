fcdae92475802e7bfd049cdad0a0abeb1db1aa42
Lubos Vondra <lubos.vondra@firma.seznam.cz>
2020-09-25 11:26:18 +0200

    add include

--- src/tengregex.h
+++ src/tengregex.h
@@ -41,6 +41,7 @@
 
 #include <string>
 #include <memory>
+#include <stdexcept>
 #include <vector>
 
 #include <pcre2.h>
@@ -618,4 +619,3 @@ RegexMatch_t RegexMatch_t::next_after_non_empty(size_type next_start_i) const {
 } // namespace Teng
 
 #endif /* TENG_TENGREGEX_H */
-
